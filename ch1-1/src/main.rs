3c15a4cf064deada993c66aa950c231a25c312d8
KLag-SF <k.lagopus.sf19@gmail.com>
2022-07-14 13:51:15 +0900

    add ch1-1

---
+++ ch1-1/src/main.rs
@@ -0,0 +1,31 @@
+fn main() {
+    let penguin_data = "\
+    common name, length(c&m)
+    Little penguin,33
+    Yellow-Eyed penguin, 65
+    ";
+
+    let records = penguin_data.lines();
+
+    for (i, record) in records.enumerate() {
+        if i == 0 || record.trim().len() == 0 {
+            continue;
+        }
+
+        let fields: Vec<_> = record
+            .split(',')
+            .map(|field| field.trim())
+            .collect();
+        
+        if cfg!(debug_assertions){
+            eprintln!("debug: {:?} -> {:?}", record, fields);
+        }
+
+        let name = fields[0];
+        if let Ok(length) = fields[1].parse::<f32>() {
+           println!("{}, {}cm", name, length); 
+        }
+    }
+
+
+}
